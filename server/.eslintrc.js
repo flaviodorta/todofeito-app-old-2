ea67dec4b1da7db56606660715da7018a1a5008a
flaviodorta <dorta.dev@gmail.com>
2022-09-23 00:03:36 -0300

    feat: add typescript, nodemon, prettier and eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single'],
+    'no-console': 1,
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+};
---
+++ server/commands.md
@@ -0,0 +1,8 @@
+yarn add --dev typescript
+./node_modules/.bin/tsc
+./node_modules/.bin/tsc --init
+yarn add --dev nodemon
+yarn add --dev ts-node
+yarn add --dev prettier
+yarn add --dev eslint
+npx eslint --init
